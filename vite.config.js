7a8a418b418e7a95916efd8c26580e7a0f3eb14d
Mohsin <mk773404@gmail.com>
2026-05-29 15:45:41 +0500

    vite.config.js updated

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:'./'
+  base:'/rock_paper_game_usereducer_gh_pages/'
 })
